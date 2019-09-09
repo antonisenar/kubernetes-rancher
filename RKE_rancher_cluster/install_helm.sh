140878b3b7f0a9513f4c5a7d58a42ff6f5f944b5
antonisenar <50085506+antonisenar@users.noreply.github.com>
2019-09-09 23:28:04 +0200

    install_helm.sh
    Instala Helm para la gestión del cluster RKE

---
+++ RKE_rancher_cluster/install_helm.sh
@@ -0,0 +1,4 @@
+kubectl -n kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
+kubectl -n kube-system rollout status deploy/tiller-deploy
